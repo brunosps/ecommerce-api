0370d5146b40ab4e9a9c20a09cfb582f249f8d0f
Bruno Santos <brunosps00@gmail.com>
2021-05-31 22:48:06 -0300

    Alterado locale para pt-BR

--- config/application.rb
+++ config/application.rb
@@ -20,21 +20,24 @@ require "action_cable/engine"
 Bundler.require(*Rails.groups)
 
 module App
-  class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    class Application < Rails::Application
+        # Initialize configuration defaults for originally generated Rails version.
+        config.load_defaults 6.1
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+        config.i18n.load_path += Dir[Rails.root.join("config/locales/**/*.{rb,yml}")]
+        config.i18n.default_locale = :'pt-BR'
 
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
-  end
+        # Configuration for the application, engines, and railties goes here.
+        #
+        # These settings can be overridden in specific environments using the files
+        # in config/environments, which are processed later.
+        #
+        # config.time_zone = "Central Time (US & Canada)"
+        # config.eager_load_paths << Rails.root.join("extras")
+
+        # Only loads a smaller set of middleware suitable for API only apps.
+        # Middleware like session, flash, cookies can be added back manually.
+        # Skip views, helpers and assets when generating a new resource.
+        config.api_only = true
+    end
 end
