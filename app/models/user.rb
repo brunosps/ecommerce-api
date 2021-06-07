240ce5ebf2a2a10633b3a4bf1042b94127d3e1bd
Bruno Santos <brunosps00@gmail.com>
2021-06-07 14:47:59 -0300

    Ajustes validação model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,12 @@
 #  index_users_on_uid_and_provider      (uid,provider) UNIQUE
 #
 class User < ActiveRecord::Base
-    devise :database_authenticatable, :registerable,
-           :recoverable, :rememberable, :validatable
-    include DeviseTokenAuth::Concerns::User
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  include DeviseTokenAuth::Concerns::User
+
+  validates :name, presence: true
+  validates :profile, presence: true
+
+  enum profile: { admin: 0, client: 1 }
 end
