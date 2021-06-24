6026293dd9465e7082d2c31711b6523655f9a9b7
Bruno Santos <brunosps00@gmail.com>
2021-06-23 22:46:26 -0300

    Adicionado testes para permissão de acesso

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,16 +5,17 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require "rspec/rails"
 
 Dir[Rails.root.join("spec", "support", "**", "*.rb")].each { |f| require f }
+Dir[Rails.root.join("spec", "shared_examples", "**", "*.rb")].each { |f| require f }
 
 begin
-    ActiveRecord::Migration.maintain_test_schema!
+  ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
-    puts e.to_s.strip
-    exit 1
+  puts e.to_s.strip
+  exit 1
 end
 RSpec.configure do |config|
-    config.fixture_path = "#{::Rails.root}/spec/fixtures"
-    config.use_transactional_fixtures = true
-    config.infer_spec_type_from_file_location!
-    config.filter_rails_from_backtrace!
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
 end
